9235c6ac63de27cd5eec35138319122bfb3138a4
Tech Tracker <31301404+techtracker5@users.noreply.github.com>
2024-07-27 11:28:59 +0530

    Delete wp-content/plugins directory

--- wp-content/plugins/wptools/dashboard/gauge.php
+++
@@ -1,6 +0,0 @@
-<center>
-<div id="wptools_myValues2"></div>
-</center>
-<script type="text/javascript">jQuery(document).ready(function ($) {
-  	$("#wptools_myValues2").speedometer({initVal:55});
-    });</script>
